896d9192efd8a63b75cd7d945e2097d6b5850334
Joao <jho_jbbantonio11@live.com>
2026-03-17 20:51:59 -0300

    fix solve historic

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import createItem from "./services/item.js";
+import * as cartService from './services/cart.js'
+
+const myCart = [];
+
+console.log ("Welcome to the your Shopee Cart!");
+
+const item1 = await createItem("hotwheels ferrari", 20.99, 1);
+const item2 = await createItem("carrinho fiat uno mille", 39.99, 3);
+
+
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+await cartService.displaycart(myCart)
+
+// await cartService.deleteItem(myCart, item1.name);
+// await cartService.deleteItem(myCart, item2.name);
+
+await cartService.removeItem(myCart, 2)
+
+await cartService.displaycart(myCart)
+
+
+await cartService.calculateTotal(myCart);
+
+
+
---
+++ src/services/cart.js
@@ -0,0 +1,62 @@
+//quais ações meu carrinho pode fazer?
+
+//casos de uso
+
+// adicionar item no carrinho
+async function addItem(userCart, item){
+    userCart.push(item);
+}
+
+
+// deletar item do carrinho
+async function deleteItem(userCart, name){
+    const index = userCart.findIndex((item) => item.name === name);
+
+    if(index !== -1){
+        userCart.splice(index, 1);
+    }
+
+}
+
+// remover um item
+async function removeItem(userCart, index)
+{
+    const indexFound = userCart.findIndex((p) =>p.name === item.name)
+
+    if(indexFound == -1){
+        console.log("item não encontrado")
+        return;
+    }
+
+    if(userCart[indexFound].quantity > 1){
+        userCart[indexFound].quantity -= 1
+        return;
+    } 
+
+    if(userCart[indexFound].quantity == 1){
+        userCart.splice(indexFound, 1)
+        return;
+    }
+}
+
+
+// calcular total
+async function calculateTotal(userCart) {
+    console.log('Shopee cart total is: ')
+    const result = userCart.reduce((total, item) => total + item.subtotal(),0);
+    console.log(result)
+}
+
+async function displaycart(userCart){
+    console.log("Shoppe cart list: ")
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity} | Subtotal = ${item.subtotal()}`);
+    });
+
+
+}
+
+
+export {
+    addItem, calculateTotal, removeItem, deleteItem, displaycart
+}
